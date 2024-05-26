26774d5e5a49e82cf678fe800581c15d7d017397
Adi Booker <adib4324@gmail.com>
2024-05-26 15:32:42 +0300

    cleanup

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ require("dotenv").config();
 
 const Mission = require("./models/Mission");
 
+// Connect to MongoDB using the URI from the .env file
 mongoose
 	.connect(process.env.MONGODB_URI)
 	.then(() => console.log("MongoDB connected"))
 	.catch((err) => console.log(err));
 
+// Define your type definitions (schema)
 const typeDefs = gql`
 	type Mission {
 		id: ID!
@@ -50,6 +52,7 @@ const typeDefs = gql`
 	}
 `;
 
+// Define your resolvers
 const resolvers = {
 	Query: {
 		missions: async () => await Mission.find(),
@@ -68,7 +71,12 @@ const resolvers = {
 	},
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// Create an Apollo server
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	persistedQueries: false, // Disable persisted queries
+});
 
 const app = express();
 server.start().then(() => {
@@ -78,3 +86,83 @@ server.start().then(() => {
 		console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
 	);
 });
+// const express = require("express");
+// const { ApolloServer, gql } = require("apollo-server-express");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const Mission = require("./models/Mission");
+
+// mongoose
+// 	.connect(process.env.MONGODB_URI)
+// 	.then(() => console.log("MongoDB connected"))
+// 	.catch((err) => console.log(err));
+
+// const typeDefs = gql`
+// 	type Mission {
+// 		id: ID!
+// 		url: String
+// 		name: String!
+// 		description: String!
+// 		startDate: String!
+// 		endDate: String
+// 		launchSite: String
+// 		destination: String
+// 	}
+
+// 	type Query {
+// 		missions: [Mission]
+// 	}
+
+// 	type Mutation {
+// 		addMission(
+// 			url: String
+// 			name: String!
+// 			description: String!
+// 			startDate: String!
+// 			endDate: String
+// 			launchSite: String
+// 			destination: String
+// 		): Mission
+// 		updateMission(
+// 			id: ID!
+// 			url: String
+// 			name: String!
+// 			description: String!
+// 			startDate: String!
+// 			endDate: String
+// 			launchSite: String
+// 			destination: String
+// 		): Mission
+// 		deleteMission(id: ID!): Mission
+// 	}
+// `;
+
+// const resolvers = {
+// 	Query: {
+// 		missions: async () => await Mission.find(),
+// 	},
+// 	Mutation: {
+// 		addMission: async (parent, args) => {
+// 			const newMission = new Mission({ ...args });
+// 			return await newMission.save();
+// 		},
+// 		updateMission: async (parent, { id, ...args }) => {
+// 			return await Mission.findByIdAndUpdate(id, args, { new: true });
+// 		},
+// 		deleteMission: async (parent, { id }) => {
+// 			return await Mission.findByIdAndDelete(id);
+// 		},
+// 	},
+// };
+
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+// const app = express();
+// server.start().then(() => {
+// 	server.applyMiddleware({ app });
+
+// 	app.listen({ port: 4000 }, () =>
+// 		console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+// 	);
+// });
